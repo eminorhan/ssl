941e9f9f66c50c0a1a2a40ff48a01528d3ef9c77
eminorhan <eo41@nyu.edu>
2021-07-27 14:11:43 -0400

    add dino

---
+++ load_dino_model.py
@@ -0,0 +1,33 @@
+from collections import OrderedDict
+
+def load_dino_model(model, checkpoint_path, verbose=False):
+    """
+    Args:
+        model (a torchvision model): Initial model to be filled.
+        checkpoint_path (path): path where the pretrained model checkpoint is stored.
+
+    Returns:
+        the filled model.
+    """
+    checkpoint = torch.load(checkpoint_path)
+    student_state_dict = checkpoint['student']
+    new_student_state_dict = OrderedDict()
+
+    if verbose:
+        print('=== Initial model state dict keys ===')
+        print(model.state_dict().keys())
+
+    for key in model.state_dict().keys():
+        
+        if 'module.backbone.' + key in student_state_dict.keys():
+            new_student_state_dict[key] = student_state_dict['module.backbone.' + key]
+            if verbose:
+                print('Parameter', key, 'taken from the pretrained model')
+        else:    
+            new_student_state_dict[key] = model.state_dict()[key]
+            if verbose:
+                print('Parameter', key, 'taken from the random init')
+
+    model.load_state_dict(new_student_state_dict)
+
+    return model
--- scripts/finetune_imgnet.sh
+++ scripts/finetune_imgnet.sh
@@ -17,7 +17,7 @@ module load cuda-10.2
 #python -u /misc/vlgscratch4/LakeGroup/emin/baby-vision/imagenet_finetuning.py --freeze-trunk --n_out 1000 --resume 'ft_IN_TC_ALL_5fps_288s_resnext50_32x4d_epoch_34.tar'
 #python -u /misc/vlgscratch4/LakeGroup/emin/baby-vision/imagenet_finetuning.py --frac-retained 0.05 --n_out 1000 --resume ''
 
-python -u /misc/vlgscratch4/LakeGroup/emin/ssl/finetune_imgnet.py --freeze-trunk --n_out 16279 --resume 'TC_SAYAVAKEPICUT_resnext101_32x8d_1.0_epoch_7.tar'
+python -u /misc/vlgscratch4/LakeGroup/emin/ssl/finetune_imgnet.py --freeze-trunk --n_out 16279 --resume 'TC_SAYAVAKEPICUT_resnext101_32x8d_1.0_epoch_9.tar'
 #python -u /misc/vlgscratch4/LakeGroup/emin/ssl/finetune_imgnet.py --freeze-trunk --n_out 1368 --resume 'TC_smallercrop_SAYAVAKEPICUT_resnext101_32x8d_0.1_epoch_9.tar'
 #python -u /misc/vlgscratch4/LakeGroup/emin/ssl/finetune_imgnet.py --freeze-trunk --n_out 137 --resume 'TC_smallercrop_SAYAVAKEPICUT_resnext101_32x8d_0.01_epoch_9_run_3.tar'
 #python -u /misc/vlgscratch4/LakeGroup/emin/ssl/finetune_imgnet.py --freeze-trunk --n_out 14 --resume 'TC_smallercrop_SAYAVAKEPICUT_resnext101_32x8d_0.001_epoch_9_run_3.tar'
